3d5962a93ca8127da29678d5329a032779f077bf
richardwxn <richardwxn@gmail.com>
2015-05-05 14:42:11 -0500

    edit

--- splitcsv.py
+++ splitcsv.py
@@ -1,5 +1,7 @@
 __author__ = 'newuser'
 import twitter
+import numpy as np
+import threading
 api = twitter.Api(consumer_key='fZhoY2TMHIS9ODgw5HjrdppPB',
                       consumer_secret='dABHXhNz2qA551hcMBcRgDZgRSy0PZ9otgW4BjEuDLYWmiwuZX',
                       access_token_key='623527276-cstgcL5iD8TSrTW7i9MtcAmaTggZBQHzE2F3Rc51',
@@ -26,20 +28,58 @@ with open("/Users/newuser/Downloads/data.1.4.tsv") as tsv:
 # create a dataframe object. pandas is a python
 # library for data analysis, kind of like R.
 import pandas as pd
+import pandas.io.parsers
+
+# global data
+# data=pandas.io.parsers.read_csv("/Users/newuser/Downloads/data.1.1.tsv",sep='\t')
+# pd.DataFrame.from_csv
 data = pd.DataFrame(data=rows)
 file2=open('/Users/newuser/Desktop/CS410/input2.txt','w')
 # print(data[4][1])
+# userid = numpy.array(data[2])
+# for index in range(len(data[1])):
 
-for index in range(len(data[1])):
+def insertdata(index):
+            i=index*10
+            # nima=api.GetUser(screen_name=','.join(data[3][i:10*(index+1)]))
 
-            if index==0:
+            # nima=api.GetStatus(id=','.join(data[2][i:10*(index+1)]))
+            # print(np.array(data[2][i:10*(index+1)]))
+            nima=api.GetStatus(id=data[2][i:10*(index+1)])
+            print(str(nima.text))
+            # file2.write(str(nima.id)+"\t")
+            # file2.write(data[1][index]+"\n")
+
+def calculate():
+    for i in xrange(len(data[1])/10+1):
+            if i==0:
                 continue
             try:
-                nima=api.GetUser(screen_name=data[2][index])
-            except twitter.TwitterError:
+                insertdata(i)
+            except twitter.TwitterError as cao:
+                print(str(cao))
                 continue
-            file2.write(str(nima.id)+"\t")
-            file2.write(data[1][index]+"\n")
+
+calculate()
+# class myThread(threading.Thread):
+#     def __init__(self, threadID):
+#         threading.Thread.__init__(self)
+#         self.threadID=threadID
+#     def run(self):
+#         calculate()
+#
+# thread1=myThread(1)
+# thread2=myThread(2)
+# thread1.start()
+# thread2.start()
+# thread1.join()
+# thread2.join()
+# t1 = threading.Thread(target=calculate, args=(5,))
+# t2 = threading.Thread(target=calculate, args=(8,))
+# t1.start()
+# t2.start()
+# t1.join()
+# t2.join()
 
 
     # bleh = str(inst)
@@ -61,7 +101,7 @@ for index in range(len(data[1])):
 # assert len(part1) + len(part2)+len(part3)+len(part4)== len(data)
 #
 # # save to excel format
-# part1.to_csv('/Users/newuser/Downloads/data.1.1.tsv', sep='\t')
+# part1.to_csvc
 # part2.to_csv('/Users/newuser/Downloads/data.1.2.tsv', sep='\t')
 # part3.to_csv('/Users/newuser/Downloads/data.1.3.tsv', sep='\t')
 # part4.to_csv('/Users/newuser/Downloads/data.1.4.tsv', sep='\t')
